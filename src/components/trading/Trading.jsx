a1e18aac8c0a4d675c603b73588b56449325efc2
Andrew <goodman.drew1@gmail.com>
2024-04-26 07:58:27 -0500

    put and delete working for sell

--- src/components/trading/Trading.jsx
+++ src/components/trading/Trading.jsx
@@ -1,193 +1,18 @@
-/*import { Button, Container, Row, Col, Card, Form } from "react-bootstrap";
-import {
-  getBrokerByUserId,
-  getClientsByBrokerId,
-} from "../../services/clientServices.jsx";
-
-import { Positions } from "../clients/Positions.jsx";
-import { useEffect, useState } from "react";
-import { buyStock, getStocks } from "../../services/equityServices.jsx";
-
-export const Trading = ({ currentUser }) => {
-  const [broker, setBroker] = useState({});
-  const [clients, setClients] = useState([]);
-
-  const [selectedClient, setSelectedClient] = useState(null);
-  const [tickerSymbol, setTickerSymbol] = useState("");
-  const [numberOfShares, setNumberOfShares] = useState("");
-
-  const [stocks, setStocks] = useState([]);
-
-  //for current broker
-  useEffect(() => {
-    if (currentUser.id) {
-      getBrokerByUserId(currentUser.id).then((data) => {
-        const brokerObject = data[0];
-        setBroker(brokerObject);
-      });
-    }
-  }, [currentUser.id]);
-
-  //for current broker's clients
-  useEffect(() => {
-    if (broker.id) {
-      getClientsByBrokerId(broker.id).then((brokerClients) => {
-        setClients(brokerClients);
-      });
-    }
-  }, [broker.id]);
-
-  //for stocks available
-  useEffect(() => {
-    if (stocks) {
-      getStocks().then((currentStocks) => {
-        setStocks(currentStocks);
-      });
-    }
-  }, []);
-
-  //handle dropdown and form
-  const handleClientSelect = (clientId) => {
-    setSelectedClient(clientId);
-  };
-
-  const handleTickerSymbolChange = (e) => {
-    setTickerSymbol(e.target.value);
-  };
-
-  const handleNumberOfSharesChange = (e) => {
-    setNumberOfShares(e.target.value);
-  };
-
-  const handleBuy = (stockId) => {
-    console.log(stockId);
-    const newPosition = {
-      shares: numberOfShares,
-      purchasePrice: 1,
-      clientId: parseInt(selectedClient),
-      stockId: stockId,
-      typeID: 1,
-    };
-    buyStock(newPosition);
-    console.log("Buy: Ticker Symbol:", tickerSymbol);
-    console.log("Buy: Number of Shares:", numberOfShares);
-
-    // Reset form fields
-    setTickerSymbol("");
-    setNumberOfShares("");
-  };
-
-  const handleSell = () => {
-    console.log("Sell: Ticker Symbol:", tickerSymbol);
-    console.log("Sell: Number of Shares:", numberOfShares);
-    // Reset form fields
-    setTickerSymbol("");
-    setNumberOfShares("");
-  };
-
-  return (
-    <Container fluid>
-      <Row>
-        <Col>
-          <h1>Select a Client</h1>
-          <Form.Select onChange={(e) => handleClientSelect(e.target.value)}>
-            <option>Select a client...</option>
-            {clients.map((client) => (
-              <option key={client.id} value={client.id}>
-                {client.user.fullName}
-              </option>
-            ))}
-          </Form.Select>
-        </Col>
-      </Row>
-      {selectedClient && (
-        <Row>
-          <Col>
-            <h2>Client Details</h2>
-            <Card className="mb-3">
-              <Card.Body>
-                {selectedClient && (
-                  <>
-                    {(() => {
-                      const client = clients.find(
-                        (client) => client.id === parseInt(selectedClient)
-                      );
-                      if (client) {
-                        return (
-                          <>
-                            <Card.Text>
-                              <strong>Selected Client:</strong>{" "}
-                              {client.user?.fullName}
-                            </Card.Text>
-                            <Card.Text>
-                              <strong>Email:</strong> {client.user?.email}
-                            </Card.Text>
-                            <Card.Text>
-                              <strong>Cash:</strong> $
-                              {client.cash.toLocaleString()}
-                            </Card.Text>
-                            <Card.Text>
-                              <strong>Positions:</strong>{" "}
-                              <Positions clientId={selectedClient} />
-                            </Card.Text>
-                          </>
-                        );
-                      }
-                      return null;
-                    })()}
-                  </>
-                )}
-              </Card.Body>
-            </Card>
-            <h2>Buy/Sell Form</h2>
-            <Form>
-              <Form.Group controlId="tickerSymbol">
-                <Form.Label>Ticker Symbol</Form.Label>
-                <Form.Select
-                  value={tickerSymbol}
-                  onChange={(e) => handleTickerSymbolChange(e.target.value)}
-                >
-                  <option value="">Select a ticker symbol...</option>
-                  {stocks.map((stock) => (
-                    <option key={stock.id} value={stock.id}>
-                      {stock.ticker}
-                    </option>
-                  ))}
-                </Form.Select>
-              </Form.Group>
-              <Form.Group controlId="numberOfShares">
-                <Form.Label>Number of Shares</Form.Label>
-                <Form.Control
-                  type="number"
-                  placeholder="Enter number of shares"
-                  value={numberOfShares}
-                  onChange={handleNumberOfSharesChange}
-                />
-              </Form.Group>
-              <Button variant="success" onClick={() => handleBuy(tickerSymbol)}>
-                Buy
-              </Button>
-              <Button variant="danger" onClick={handleSell}>
-                Sell
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-      )}
-    </Container>
-  );
-};
-*/
-
 import { Button, Container, Row, Col, Card, Form } from "react-bootstrap";
 import {
   getBrokerByUserId,
   getClientsByBrokerId,
+  getPositionsByClientId,
 } from "../../services/clientServices.jsx";
 
 import { Positions } from "../clients/Positions.jsx";
 import { useEffect, useState } from "react";
-import { buyStock, getStocks } from "../../services/equityServices.jsx";
+import {
+  buyStock,
+  getStocks,
+  sellAllStock,
+  sellStock,
+} from "../../services/equityServices.jsx";
 
 export const Trading = ({ currentUser }) => {
   const [broker, setBroker] = useState({});
@@ -198,6 +23,7 @@ export const Trading = ({ currentUser }) => {
   const [tickerSymbol, setTickerSymbol] = useState(""); // Add tickerSymbol state
 
   const [stocks, setStocks] = useState([]);
+  const [positions, setPositions] = useState([]);
 
   //for current broker
   useEffect(() => {
@@ -225,6 +51,12 @@ export const Trading = ({ currentUser }) => {
     });
   }, []);
 
+  useEffect(() => {
+    getPositionsByClientId(selectedClient).then((currentPositions) => {
+      setPositions(currentPositions);
+    });
+  }, [selectedClient]);
+
   //handle dropdown and form
   const handleClientSelect = (clientId) => {
     setSelectedClient(clientId);
@@ -238,10 +70,7 @@ export const Trading = ({ currentUser }) => {
     setTickerSymbol(e.target.value);
   };
 
-  const handleBuy = () => {
-    console.log("Buy: Ticker Symbol:", tickerSymbol);
-    console.log("Buy: Number of Shares:", numberOfShares);
-
+  const handleBuy = async () => {
     const newPosition = {
       shares: parseInt(numberOfShares),
       purchasePrice: 1, // Placeholder value, replace with actual purchase price
@@ -257,11 +86,30 @@ export const Trading = ({ currentUser }) => {
   };
 
   const handleSell = () => {
-    console.log("Sell: Ticker Symbol:", tickerSymbol);
-    console.log("Sell: Number of Shares:", numberOfShares);
+    getPositionsByClientId(selectedClient).then((positions) => {
+      const sellThisStock = positions.find(
+        (position) => position.stockId === parseInt(tickerSymbol)
+      );
 
-    setNumberOfShares("");
-    setTickerSymbol(""); // Reset tickerSymbol after sell
+      if (sellThisStock) {
+        if (sellThisStock.shares === parseInt(numberOfShares)) {
+          sellAllStock(sellThisStock);
+        } else {
+          sellThisStock.shares =
+            sellThisStock.shares - parseInt(numberOfShares);
+          console.log("Found position to sell:", sellThisStock);
+          sellStock(sellThisStock);
+        }
+        // Reset form fields
+        setNumberOfShares("");
+        setTickerSymbol("");
+      } else {
+        console.log(
+          "No position found to sell for ticker symbol:",
+          tickerSymbol
+        );
+      }
+    });
   };
 
   return (
@@ -307,7 +155,10 @@ export const Trading = ({ currentUser }) => {
                             </Card.Text>
                             <Card.Text>
                               <strong>Positions:</strong>{" "}
-                              <Positions clientId={selectedClient} />
+                              <Positions
+                                clientId={selectedClient}
+                                positions={positions}
+                              />
                             </Card.Text>
                           </>
                         );
--- src/services/equityServices.jsx
+++ src/services/equityServices.jsx
@@ -1,6 +1,9 @@
 export const getStocks = () => {
   return fetch("http://localhost:8088/stocks").then((res) => res.json());
 };
+export const getPositions = (clientId) => {
+  return fetch("http://localhost:8088/stocks").then((res) => res.json());
+};
 export const buyStock = (newPosition) => {
   return fetch("http://localhost:8088/positions", {
     method: "POST",
@@ -8,5 +11,23 @@ export const buyStock = (newPosition) => {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(newPosition),
+  });
+};
+export const sellStock = (thisPosition) => {
+  return fetch(`http://localhost:8088/positions/${thisPosition.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(thisPosition),
+  });
+};
+export const sellAllStock = (thisPosition) => {
+  return fetch(`http://localhost:8088/positions/${thisPosition.id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(thisPosition),
   });
 };
